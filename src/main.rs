b8c57b0211fe638cf99da92727f53e34bf4fed10
zhouli02 <1310537426@qq.com>
2021-09-21 22:40:23 +0800

    标准输出重定向到output.txt

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use minigrep::{Config,run};
 fn main () {
     let args = env::args().collect::<Vec<String>>();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument: {}",err);
+        eprintln!("Problem parsing argument: {}",err);
         process::exit(1);
     });
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
-    }
+    } 
 }
